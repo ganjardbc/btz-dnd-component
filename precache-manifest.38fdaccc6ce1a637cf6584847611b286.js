30b2bd04cdbc164bd07890f6170c862ed57d17c6
Ganjar Hadiatna <ganjardbc@gmail.com>
2021-09-03 18:18:48 +0700

    Updates

---
+++ precache-manifest.38fdaccc6ce1a637cf6584847611b286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088bcab303f62b917e6a4ef1227f5253",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c4b909c881e7eeca2c9",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "a57bef46983454e57780",
+    "url": "./static/js/2.8b62e87f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8b62e87f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4b909c881e7eeca2c9",
+    "url": "./static/js/main.c8fb22a9.chunk.js"
+  },
+  {
+    "revision": "3a58d064c9a8e8e65b6f",
+    "url": "./static/js/runtime-main.5d5dbf40.js"
+  }
+]);
